85272c66265fef822b8fdd7be1dbfb239bef4c45
vicpril <vicpride@gmail.com>
2019-11-04 12:49:26 +0700

    front fix

--- resources/views/front/layout.blade.php
+++ resources/views/front/layout.blade.php
@@ -20,67 +20,8 @@
    </head>
 
    <body>
-      <!--    <div class="primary-overlay"> -->
 
-      <header class=" blog-header py-0">
-         <div class="container">
-            <div class="header-image">
-               <div class="primary-overlay">
-                  <div
-                     class="d-sm-flex mx-0 h-100 py-3 justify-content-between align-items-start "
-                  >
-                     <a href="{{ app('router')->has('home') ? route('home') : url('/') }}" class="logo-link">
-                       <div class="logo-title">
-                          <h1 class="main-title">@lang('Идеи и Идеалы')</h1>
-                          <h4 class="second-title">@lang('Научный журнал')</h4>
-                       </div>
-                     </a>
-
-                     <div
-                        class="dropdown form-inline bg-white px-3 border rounded"
-                     >
-                        <span class="">@lang('Язык сайта'): </span>
-                        <a
-                           class="nav-link dropdown-toggle "
-                           href="#"
-                           role="button"
-                           data-toggle="dropdown"
-                           aria-haspopup="true"
-                           aria-expanded="false"
-                        >
-                           @if( Config::get('app.locale') == "ru" )
-                           <i class="flag-icon flag-icon-ru mr-1"></i>Русский
-                           @else
-                           <i class="flag-icon flag-icon-us mr-1"></i>English
-                           @endif
-                        </a>
-                        <div class="dropdown-menu">
-                           @foreach(LaravelLocalization::getSupportedLocales()
-                           as $localeCode => $properties)
-                           <a
-                              rel="alternate"
-                              class="dropdown-item"
-                              hreflang="{{ $localeCode }}"
-                              href="{{ LaravelLocalization::getLocalizedURL($localeCode, null, [], true) }}"
-                           >
-                              <i
-                                 class="flag-icon flag-icon-{{
-                                    $properties['flag']
-                                 }} mr-1"
-                              ></i
-                              >@lang($properties['title'])
-                           </a>
-
-                           @if(!$loop->last)
-                           <div class="dropdown-divider"></div>
-                           @endif @endforeach
-                        </div>
-                     </div>
-                  </div>
-               </div>
-            </div>
-         </div>
-      </header>
+      @include('front.header') 
 
       <div class="nav-scroller py-1 mb-2 bg-white shadow">
          <div class="container">
@@ -130,7 +71,6 @@
          <!-- /.row -->
       </main>
       <!-- /.container -->
-      <!--    </div> -->
 
       @include('front.footer')
 
